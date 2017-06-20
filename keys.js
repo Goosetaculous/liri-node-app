f7944a081c4163fc7ca5301388706645781db103
Goose <josephtrop@gmail.com>
2017-06-19 18:28:36 -0700

    TST

--- keys.js
+++ keys.js
@@ -1,8 +1,12 @@
-console.log('this is loaded');
 
 exports.twitterKeys = {
-    consumer_key: '4502Ntfjaqigjfu3Y2IJ9K31X',
-    consumer_secret: 'vAtu4c3qaOojihEOdOqJIyRNiDuqkdNm51UpY7uLlXeBKS5nK6',
-    access_token_key: '636617126-vXT9baMMUgJWdvJjHnU0jkFpcnro8m5QuhZlkXyt',
-    access_token_secret: '4C9jAHMjwR8O1UYu5P2UGWSE1aM0Kdy0Svr7B88Onytcu',
+    consumer_key        : '4502Ntfjaqigjfu3Y2IJ9K31X',
+    consumer_secret     : 'vAtu4c3qaOojihEOdOqJIyRNiDuqkdNm51UpY7uLlXeBKS5nK6',
+    access_token_key    : '636617126-vXT9baMMUgJWdvJjHnU0jkFpcnro8m5QuhZlkXyt',
+    access_token_secret : '4C9jAHMjwR8O1UYu5P2UGWSE1aM0Kdy0Svr7B88Onytcu',
+}
+
+exports.spotifyKeys = {
+    id       : '06e69991ea644739bc647ba0bd64de8c',
+    secret   : 'f867edc7d2524327adb797ae701cf650'
 }
--- liri.js
+++ liri.js
@@ -6,7 +6,9 @@
     var fs       =  require('fs')
     var keys     =  require('./keys')
     var Twitter  =  require('twitter')
+    var Spotify  =  require('node-spotify-api')
     var client   =  new Twitter(keys.twitterKeys)
+    var spotify  =  new Spotify(keys.spotifyKeys)
     var command =  process.argv[2]
     fs.access('./log.txt','wx', function(err){
         err ? createLogFile(command) : null;
@@ -28,25 +30,63 @@
         })
     }
 
-
-
+    function buildCommandstring(){
+        var cmdString="";
+        for (var i = 2; i < process.argv.length; i++){
+            cmdString = cmdString  +process.argv[i] + " "
+        }
+        return cmdString
+    }
     function getCommand(command){
-        fs.appendFile('log.txt',command+":\n")
+        fs.appendFile('log.txt',buildCommandstring()+":\n",function(err){
+            err ? console.log("Something wrong with writing the file ") : null
+        })
         switch (command){
             case 'my-tweets':
                 getTweets()
                 break;
             case 'spotify-this-song':
+                 if( checkSongTitle()) {
+                     getSong(checkSongTitle())
+                 }
                 break;
             case 'movie-this':
                 break;
             case 'do-what-it-says':
                 break;
         }
-
+    }
+    function checkSongTitle() {
+        var commandStrings = ['my-tweets','spotify-this-song','movie-this','do-what-it-says']
+        if(process.argv.length > 3 && commandStrings.indexOf(process.argv[3]) === -1 ){
+            return process.argv[3]
+        }else {
+            console.log("Need a valid song title")
+            writeLogFile("Need a song title")
+            return false
+        }
     }
 
+    function getSong(song){
+        //console.log(song)
 
+        spotify.search({ type: 'track', query: song, limit:1 }, function(err, data) {
+            if (err) {
+                writeLogFile('Error occurred: ' + err)
+                return console.log('Error occurred: ' + err);
+            }
+            // Artist(s)
+            // The song's name
+            // A preview link of the song from Spotify
+            // The album that the song is from
+
+            console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name,null,2)); // NAME of the artist
+            console.log(JSON.stringify(data.tracks.items[0].name,null,2)); // name of the song
+            console.log(JSON.stringify(data.tracks.items[0].album.name,null,2)); //Name of the album
+            console.log(JSON.stringify(data.tracks.items[0].preview_url,null,2)); //preview URL
+
+        });
+    }
 
     function getTweets(){
         client.get('statuses/user_timeline.json', {count: 20}, function(error, tweets, response) {
