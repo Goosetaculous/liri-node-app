097bac33934adb02f3be29fd3fa3a18c64d8f64f
Goose <josephtrop@gmail.com>
2017-06-20 00:15:10 -0700

    updates

--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@
     var keys     =  require('./keys')
     var Twitter  =  require('twitter')
     var Spotify  =  require('node-spotify-api')
+    var request  =  require('request')
     var client   =  new Twitter(keys.twitterKeys)
     var spotify  =  new Spotify(keys.spotifyKeys)
     var command =  process.argv[2]
@@ -46,45 +47,60 @@
                 getTweets()
                 break;
             case 'spotify-this-song':
-                 if( checkSongTitle()) {
-                     getSong(checkSongTitle())
-                 }
+                getSong(checkTitle('song'))
                 break;
             case 'movie-this':
+                getMovie(checkTitle('movie'))
                 break;
             case 'do-what-it-says':
                 break;
         }
     }
-    function checkSongTitle() {
+
+    function getMovie(movie){
+        request('http://www.omdbapi.com/?apikey=40e9cece&t='+movie, function(error, response, body){
+            if(!error  && response.statusCode ==  200){
+                var obj= JSON.parse(body)
+                console.log(obj.Title)
+                console.log(obj.Year)
+                console.log(obj.imdbRating)
+                console.log(obj.Country)
+                console.log(obj.Language)
+                console.log(obj.Plot)
+                console.log(obj.Actors)
+                console.log(obj.Ratings[1].value)
+            }
+
+        })
+    }
+
+    function checkTitle(command) {
         var commandStrings = ['my-tweets','spotify-this-song','movie-this','do-what-it-says']
         if(process.argv.length > 3 && commandStrings.indexOf(process.argv[3]) === -1 ){
             return process.argv[3]
-        }else {
-            console.log("Need a valid song title")
-            writeLogFile("Need a song title")
-            return false
+        }else if(command == 'song') {
+            writeLogFile("Need a song title, so we'll provide 'The Sign'")
+            return "The Sign Ace of Base"
+        } else if(command=='movie'){
+            writeLogFile("Need a Movie title, so we'll provide 'Mr.Nobody'")
+            return "Mr.Nobody"
         }
     }
 
     function getSong(song){
-        //console.log(song)
-
         spotify.search({ type: 'track', query: song, limit:1 }, function(err, data) {
             if (err) {
                 writeLogFile('Error occurred: ' + err)
                 return console.log('Error occurred: ' + err);
             }
-            // Artist(s)
-            // The song's name
-            // A preview link of the song from Spotify
-            // The album that the song is from
-
-            console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name,null,2)); // NAME of the artist
-            console.log(JSON.stringify(data.tracks.items[0].name,null,2)); // name of the song
-            console.log(JSON.stringify(data.tracks.items[0].album.name,null,2)); //Name of the album
-            console.log(JSON.stringify(data.tracks.items[0].preview_url,null,2)); //preview URL
-
+            console.log(data.tracks.items[0].album.artists[0].name)
+            writeLogFile(data.tracks.items[0].album.artists[0].name)
+            console.log(data.tracks.items[0].name)
+            writeLogFile(data.tracks.items[0].name)
+            console.log(data.tracks.items[0].external_urls.spotify)
+            writeLogFile(data.tracks.items[0].external_urls.spotify)
+            console.log(data.tracks.items[0].album.name)
+            writeLogFile(data.tracks.items[0].album.name)
         });
     }
 
@@ -98,9 +114,5 @@
             }
         });
     }
-
     getCommand(command)
-
-
-
 })()
